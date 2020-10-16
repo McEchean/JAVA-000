ccfa673c35799a73d5779ea3f32a5fcb5d610474
HengFang Wang <wanghengfang@hyperchain.cn>
2020-10-17 02:51:53 +0800

    feat(week01): second problem

---
+++ Week_01/2/HelloClassLoader.java
@@ -0,0 +1,47 @@
+import java.io.ByteArrayOutputStream;
+import java.io.FileInputStream;
+import java.lang.reflect.Method;
+
+public class HelloClassLoader extends ClassLoader{
+    @Override
+    protected Class<?> findClass(String name) throws ClassNotFoundException {
+        byte[] classFile = getClassFile();
+        if(classFile == null) {
+            throw new ClassNotFoundException();
+        }
+        //将错的byte改为正确
+        for (int i = 0; i < classFile.length; i++) {
+            classFile[i] = (byte)(255 - (int)classFile[i]);
+        }
+        return defineClass(name,classFile, 0, classFile.length);
+    }
+
+    private byte[] getClassFile() {
+        try {
+            FileInputStream fileInputStream = new FileInputStream("./Hello.xlass");
+            ByteArrayOutputStream bos = new ByteArrayOutputStream();
+            byte[] buffer = new byte[4096];
+            int readn = 0;
+            while((readn = fileInputStream.read(buffer)) != -1) {
+                bos.write(buffer, 0, readn);
+            }
+            return bos.toByteArray();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+
+    public static void main(String[] args) {
+        HelloClassLoader hcl = new HelloClassLoader();
+        try {
+            Class<?> helloClass = hcl.findClass("Hello");
+            Method helloMethod = helloClass.getMethod("hello");
+            Object o = helloClass.newInstance();
+            helloMethod.invoke(o);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
